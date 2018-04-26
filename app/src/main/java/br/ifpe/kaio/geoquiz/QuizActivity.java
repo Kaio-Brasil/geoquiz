2586f49b403454cdc95d38a203bbe367221a068c
Kaio <kaio.gus@outlook.com>
2018-04-26 02:18:25 -0300

    GeoQuiz 3.0

--- app/src/main/java/br/ifpe/kaio/geoquiz/QuizActivity.java
+++ app/src/main/java/br/ifpe/kaio/geoquiz/QuizActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mBtnVerdadeiro;
     private Button mBtnFalso;
-    private Button mBtnAvancar;
+    private ImageButton mBtnAnterior;
+    private ImageButton mBtnProximo;
 
     private TextView mQuestoesTextView;
 
@@ -20,28 +24,45 @@ public class QuizActivity extends AppCompatActivity {
             new Questoes(R.string.pergunta3, false),
             new Questoes(R.string.pergunta4, true),
             new Questoes(R.string.pergunta5, false),
+            new Questoes(R.string.pergunta6, true),
+            new Questoes(R.string.pergunta7, true),
+            new Questoes(R.string.pergunta8, true),
+            new Questoes(R.string.pergunta9, false),
+            new Questoes(R.string.pergunta10, false)
     };
 
     int mCurrentIndex = 0;
+    int mPontuacao = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        embaralharPerguntas();
         mQuestoesTextView = (TextView) findViewById(R.id.pergunta_text_view);
-
-        int questao = mBancoPerguntas[mCurrentIndex].getTextoResId();
-        mQuestoesTextView.setText(questao);
 
         mBtnVerdadeiro = (Button) findViewById(R.id.btn_true);
         mBtnFalso = (Button) findViewById(R.id.btn_false);
-        mBtnAvancar = (Button) findViewById(R.id.btn_proximo);
+        mBtnAnterior = (ImageButton) findViewById(R.id.btn_anterior);
+        mBtnProximo = (ImageButton) findViewById(R.id.btn_proximo);
+
+        mQuestoesTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mBancoPerguntas.length != 0) {
+                    mCurrentIndex = (mCurrentIndex + 1) % mBancoPerguntas.length;
+                    atualizarPergunta();
+                }
+            }
+        });
 
         mBtnVerdadeiro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 checarResposta(true);
+                removerPerguntas();
+                mostrarPontuacao();
             }
         });
 
@@ -49,14 +70,26 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 checarResposta(true);
+                removerPerguntas();
+                mostrarPontuacao();
             }
         });
 
 
-        mBtnAvancar.setOnClickListener(new View.OnClickListener(){
+        mBtnProximo.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mBancoPerguntas.length;
+                atualizarPergunta();
+            }
+        });
+
+        mBtnAnterior.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(mCurrentIndex != 0) {
+                    mCurrentIndex--;
+                }
                 atualizarPergunta();
             }
         });
@@ -69,8 +102,57 @@ public class QuizActivity extends AppCompatActivity {
 
     private void checarResposta(boolean resultadoUsuario) {
         boolean resposta = mBancoPerguntas[mCurrentIndex].isResposta();
-        int messagemResId = (resposta == resultadoUsuario) ? R.string.correto_toast : R.string.incorreto_toast;
-        Toast.makeText(this, messagemResId, Toast.LENGTH_SHORT).show();
+        int mensagemResId;
+        //int mensagemResId = (resposta == resultadoUsuario) ? R.string.correto_toast : R.string.incorreto_toast;
+        if(resposta == resultadoUsuario) {
+            mensagemResId = R.string.correto_toast;
+            mPontuacao++;
+        } else {
+            mensagemResId = R.string.incorreto_toast;
+        }
+        Toast.makeText(this, mensagemResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void mostrarPontuacao() {
+        if(mBancoPerguntas.length == 0) {
+            String mensagem = "Sua pontuação foi "+ mPontuacao;
+            Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show();
+            desabilitaBotoes();
+        }
+    }
+
+    private void desabilitaBotoes() {
+        mBtnVerdadeiro.setEnabled(false);
+        mBtnFalso.setEnabled(false);
+        mBtnAnterior.setEnabled(false);
+        mBtnProximo.setEnabled(false);
+    }
+
+    private void embaralharPerguntas() {
+        for(int i=0; i<(mBancoPerguntas.length - 1); i++) {
+            //Recebe um index aleatorio
+            int j = new Random().nextInt(mBancoPerguntas.length);
+
+            //Troca o conteudo das questões
+            Questoes temporario = mBancoPerguntas[i];
+            mBancoPerguntas[i] = mBancoPerguntas[j];
+            mBancoPerguntas[j] = temporario;
+        }
+    }
+
+    private void removerPerguntas() {
+        if(mBancoPerguntas.length != 0) {
+            Questoes[] arrAuxiliar = new Questoes[mBancoPerguntas.length - 1];
+            int index = 0;
+
+            for (int i = 0; i<mBancoPerguntas.length; i++) {
+                if (i != mCurrentIndex) {
+                    arrAuxiliar[index] = mBancoPerguntas[i];
+                    index++;
+                }
+            }
+            mBancoPerguntas = arrAuxiliar;
+        }
     }
 
 }
